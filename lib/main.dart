f6e7b3462c92a34edef61ac2e28ace71cf0a33bf
Cristian Lopez <lopezcristianj@gmail.com>
2019-12-02 20:19:32 +1300

    Updating app to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,41 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 5;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = 3;
+                });
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = 2;
+                });
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
