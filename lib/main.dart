690bcd32163c27abe634bf82220f25d7441254e1
Cristian Lopez <lopezcristianj@gmail.com>
2019-12-02 21:26:25 +1300

    set State in separate function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -21,7 +22,15 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePageState extends State<DicePage> {
-  var leftDiceNumber = 5;
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
+  void updateRandomValues() {
+    setState(() {
+      rightDiceNumber = Random().nextInt(6) + 1;
+      leftDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,9 +40,7 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                setState(() {
-                  leftDiceNumber = 3;
-                });
+                updateRandomValues();
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -41,11 +48,9 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                setState(() {
-                  leftDiceNumber = 2;
-                });
+                updateRandomValues();
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           )
         ],
